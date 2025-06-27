ebc88a40aca869f3d4c3ed7fe5f9d88e19791b6c
naruegsantos <naruegsantos@gmai.com>
2025-06-27 09:02:06 -0300

    deploy: remove /.next folder

--- apps/web/.next/server/app/favicon.ico/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/favicon.ico/route.js";
-const runtime = require("../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/23774_next_072fa964._.js");
-runtime.loadChunk("server/chunks/[root of the server]__836d6541._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/apps/web/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.2.3_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/apps/web/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.2.3_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/apps/web/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH).exports;

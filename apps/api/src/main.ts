84931c6b48bc7afdbe8b1fac628dbd064a6c8620
naruegsantos <naruegsantos@gmai.com>
2025-06-27 08:26:16 -0300

    deploy: remove env import in api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { env } from "@/../../packages/env/src/index";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(env.PORT ?? 8000);
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
